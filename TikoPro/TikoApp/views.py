c6d8a985c217661fd332a03f8f6428d483986575
patrickNgare <patrickngare@gmail.com>
2018-03-21 15:36:52 +0300

    create a function to pull all the events

--- TikoPro/TikoApp/views.py
+++ TikoPro/TikoApp/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from . models import Profile,EventType,Event
 
 
 
 def index(request):
     
+    events= Event.objects.all()
+    print(events)
 
-    return render(request,'temps/index.html')
+    return render(request,'temps/index.html',{"events": events})
