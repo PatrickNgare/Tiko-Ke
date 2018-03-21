8ed1f8a87e79f42cd889fd711000857245ffe009
patrickNgare <patrickngare@gmail.com>
2018-03-21 15:36:26 +0300

    changed category model to event model

--- TikoPro/TikoApp/models.py
+++ TikoPro/TikoApp/models.py
@@ -15,20 +15,29 @@ class Profile(models.Model):
 
 
 # events category model/fileds
-class Category(models.Model):
+class EventType(models.Model):
     name=models.CharField(max_length=150,)
+    
+    def __str__(self):
+        return self.name
+
 
 class Event(models.Model):
-    category=models.ForeignKey(Category,related_name='events',on_delete=models.CASCADE)
+    eventtype=models.ForeignKey(EventType,related_name='events',on_delete=models.CASCADE)
     name=models.CharField(max_length=100,)
     description=models.TextField()
-    price=models.DecimalField(max_digits=10,decimal_places=2)
+    price=models.DecimalField(max_digits=10,decimal_places=2,default=0.00)
     available=models.BooleanField(default=True)
     tickets= models.PositiveIntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     image=models.ImageField(upload_to='gallery/',blank=True,null=True)
+    location = models.CharField(max_length=30, blank=True)
     Event_date=models.DateField()
     Event_time=models.TimeField()
+
+    def __str__(self):
+        return self.name
+        
 
 
 
