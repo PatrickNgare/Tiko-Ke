7ac257791363968196a52fe0ad22f9bd1bf58c53
patrickNgare <patrickngare@gmail.com>
2018-03-21 15:35:58 +0300

    changed the category model to events type

--- TikoPro/TikoApp/admin.py
+++ TikoPro/TikoApp/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Profile,Category,Event
+from .models import Profile,EventType,Event
 
 # Register your models here.
 admin.site.register(Profile)
-admin.site.register(Category)
+admin.site.register(EventType)
 admin.site.register(Event)
 
