a1ab2b42fe08cf81a0f775f7f491c54bbbdd6c21
patrickNgare <patrickngare@gmail.com>
2018-03-22 11:04:52 +0300

    addd description function in views to pull single event

--- TikoPro/TikoApp/views.py
+++ TikoPro/TikoApp/views.py
@@ -6,6 +6,17 @@ from . models import Profile,EventType,Event
 def index(request):
     
     events= Event.objects.all()
-    print(events)
-
+    
     return render(request,'temps/index.html',{"events": events})
+
+
+def description(request,id):
+
+
+    single=Event.objects.get(id=id)
+
+    print(single)
+
+
+
+    return render(request ,'temps/descritpion.html',{"single":single})
