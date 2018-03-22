d982c0726ac55b758987ba8691eb589e15eb13b3
patrickNgare <patrickngare@gmail.com>
2018-03-22 11:03:24 +0300

    registerd tags in the admin py

--- TikoPro/TikoApp/admin.py
+++ TikoPro/TikoApp/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from .models import Profile,EventType,Event
+from .models import Profile,EventType,Event,Tags
 
 # Register your models here.
+
+
+class EventAdmin(admin.ModelAdmin):
+    filter_horizontal =('tags',)
+
 admin.site.register(Profile)
 admin.site.register(EventType)
-admin.site.register(Event)
-
+admin.site.register(Event,EventAdmin)
+admin.site.register(Tags)
