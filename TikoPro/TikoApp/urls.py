5e6678e2edd0966c053c668a8fa6bd817f09eb56
patrickNgare <patrickngare@gmail.com>
2018-03-23 08:53:28 +0300

    added search urls

--- TikoPro/TikoApp/urls.py
+++ TikoPro/TikoApp/urls.py
@@ -6,7 +6,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
    url(r'^$',views.index, name='index'),
-   url(r'^description/(\d+)',views.description,name='description')
+   url(r'^description/(\d+)',views.description,name='description'),
+   url(r'^search/', views.search, name='search_results'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
